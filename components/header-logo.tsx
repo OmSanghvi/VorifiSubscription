c8be11d16730ac470f378a3087b161b294abb2d0
OmSanghvi <142817534+OmSanghvi@users.noreply.github.com>
2024-09-14 14:30:13 -0600

    Update header-logo.tsx

--- components/header-logo.tsx
+++ components/header-logo.tsx
@@ -6,7 +6,7 @@ export const HeaderLogo = () => {
             <div className='items-start justify-start hidden lg:flex'>
                 <Image src = "/logo.svg" alt = "Logo" height={28} width={28}/>
                 <p className='font-semibold text-white text-2xl ml-2.5'>
-                   Finance 
+                   Vorifi 
                 </p>
             </div>
         </Link>
