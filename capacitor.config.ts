19f300bebf49a6060c01376fe55bf5105b55ff68
Om Sanghvi <omsanghvi1@gmail.com>
2024-11-13 20:07:16 -0700

    Deleted whitespace

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.yourapp.identifier',
-  appName: 'Your App Name',
-  webDir: 'public'
-};
-
-export default config;
