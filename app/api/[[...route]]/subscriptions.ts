8f819b0a9320044095a1973b48f547f410db6fc7
Om Sanghvi <omsanghvi1@gmail.com>
2025-01-07 19:04:36 -0700

    LemonSqueezy

--- app/api/[[...route]]/subscriptions.ts
+++ app/api/[[...route]]/subscriptions.ts
@@ -1,144 +1,153 @@
-import crypto from 'crypto';
-import { Hono } from 'hono';
-import { eq } from 'drizzle-orm';
-import { createId } from '@paralleldrive/cuid2';
-import { clerkMiddleware, getAuth } from '@hono/clerk-auth';
-import { createCheckout, getSubscription } from '@lemonsqueezy/lemonsqueezy.js';
+import crypto from "crypto";
+import { Hono } from "hono";
+import { eq } from "drizzle-orm";
 
-import { db } from '@/db/drizzle';
-import { setupLemon } from '@/lib/ls';
-import { subscriptions } from '@/db/schema';
+import {
+    clerkMiddleware,
+    getAuth
+} from "@hono/clerk-auth";
+import {
+    createCheckout,
+    getSubscription
+} from "@lemonsqueezy/lemonsqueezy.js";
+
+import { db } from "@/db/drizzle";
+import { setupLemon } from "@/lib/ls";
+import { subscriptions } from "@/db/schema";
+import { createId } from "@paralleldrive/cuid2";
 
 setupLemon();
 
 const app = new Hono()
-    .get('/current', clerkMiddleware(), async (c) => {
-        const auth = getAuth(c);
-        if (!auth?.userId) {
-            return c.json({ error: 'Unauthorized' }, 401);
-        }
+    .get(
+        "/current",
+        clerkMiddleware(),
+        async (c) => {
+            const auth = getAuth(c);
 
-        const [subscription] = await db
-            .select()
-            .from(subscriptions)
-            .where(eq(subscriptions.userId, auth.userId));
-
-        return c.json({ data: subscription || null });
-    })
-    .post('/checkout', clerkMiddleware(), async (c) => {
-        const auth = getAuth(c);
-
-        if (!auth?.userId) {
-            return c.json({ error: 'Unauthorized' }, 401);
-        }
-
-        const [existing] = await db
-            .select()
-            .from(subscriptions)
-            .where(eq(subscriptions.userId, auth.userId));
-
-        if (existing?.subscriptionId) {
-            const subscription = await getSubscription(existing.subscriptionId);
-            const portalUrl = subscription.data?.data.attributes.urls.customer_portal;
-
-            if (!portalUrl) {
-                return c.json({ error: 'Internal error' }, 500);
+            if (!auth?.userId) {
+                return c.json({ error: "Unauthorized" }, 401);
             }
 
-            return c.json({ data: portalUrl });
-        }
+            const [subscription] = await db
+                .select()
+                .from(subscriptions)
+                .where(
+                    eq(subscriptions.userId, auth.userId),
+                );
 
-        const checkout = await createCheckout(
-            process.env.LEMONSQUEEZY_STORE_ID!,
-            process.env.LEMONSQUEEZY_PRODUCT_ID!,
-            {
-                checkoutData: {
-                    custom: {
-                        user_id: auth.userId,
+            return c.json({ data: subscription || null });
+        },
+    )
+    .post(
+        "/checkout",
+        clerkMiddleware(),
+        async (c) => {
+            const auth = getAuth(c);
+
+            if (!auth?.userId) {
+                return c.json({ error: "Unauthorized" }, 401);
+            }
+
+            const [existing] = await db
+                .select()
+                .from(subscriptions)
+                .where(
+                    eq(subscriptions.userId, auth.userId),
+                );
+
+            if (existing?.subscriptionId) {
+                const subscription = await getSubscription(
+                    existing.subscriptionId,
+                );
+
+                const portalUrl = subscription.data?.data.attributes.urls.customer_portal;
+
+                if (!portalUrl) {
+                    return c.json({ error: "Internal error" }, 500);
+                }
+
+                return c.json({ data: portalUrl });
+            }
+
+            const checkout = await createCheckout(
+                process.env.LEMONSQUEEZY_STORE_ID!,
+                process.env.LEMONSQUEEZY_PRODUCT_ID!,
+                {
+                    checkoutData: {
+                        custom: {
+                            user_id: auth.userId,
+                        },
                     },
+                    productOptions: {
+                        redirectUrl: `${process.env.NEXT_PUBLIC_APP_URL!}/`,
+                    }
                 },
-                productOptions: {
-                    redirectUrl: `${process.env.NEXT_PUBLIC_APP_URL!}/`,
-                },
+            );
+
+            console.log({ checkout })
+            const checkoutUrl = checkout.data?.data.attributes.url;
+
+            if (!checkoutUrl) {
+                return c.json({ error: "Internal error" }, 500);
             }
-        );
 
-        const checkoutUrl = checkout.data?.data.attributes.url;
+            return c.json({ data: checkoutUrl });
+        },
+    )
+    .post(
+        "/webhook",
+        async (c) => {
+            const text = await c.req.text();
 
-        if (!checkoutUrl) {
-            return c.json({ error: 'Internal error' }, 500);
-        }
+            const hmac = crypto.createHmac(
+                "sha256",
+                process.env.LEMONSQUEEZY_WEBHOOK_SECRET!
+            );
+            const digest = Buffer.from(
+                hmac.update(text).digest("hex"),
+                "utf-8"
+            );
+            const signature = Buffer.from(
+                c.req.header("x-signature") as string,
+                "utf8"
+            );
 
-        return c.json({ data: checkoutUrl });
-    })
-    // this webhook endpoint doesn't require middleware because it's publicly available
-    .post('/webhook', async (c) => {
-        // use signature to confirm it's lemon squeezy
-        const text = await c.req.text();
+            if (!crypto.timingSafeEqual(digest, signature)) {
+                return c.json({ error: "Unauthorized" }, 401);
+            }
 
-        const hmac = crypto.createHmac(
-            'sha256',
-            process.env.LEMONSQUEEZY_WEBHOOK_SECRET!
-        );
-        const digest = Buffer.from(hmac.update(text).digest('hex'), 'utf8');
-        const signature = Buffer.from(
-            c.req.header('x-signature') as string,
-            'utf8'
-        );
+            const payload = JSON.parse(text);
+            const event = payload.meta.event_name;
 
-        if (!crypto.timingSafeEqual(digest, signature)) {
-            return c.json({ error: 'Unauthorized' }, 401);
-        }
+            const subscriptionId = payload.data.id;
+            const userId = payload.meta.custom_data.user_id;
+            const status = payload.data.attributes.status;
 
-        const payload = JSON.parse(text);
-        const event = payload.meta.event_name;
-
-        const subscriptionId = payload.data.id;
-        const userId = payload.meta.custom_data.user_id;
-        const status = payload.data.attributes.status;
-
-        const [existing] = await db
-            .select()
-            .from(subscriptions)
-            .where(eq(subscriptions.subscriptionId, subscriptionId));
-
-        if (event === 'subscription_created') {
-            if (existing) {
+            if (event === "subscription_created") {
                 await db
-                    .update(subscriptions)
-                    .set({
+                    .insert(subscriptions)
+                    .values({
+                        id: createId(),
+                        subscriptionId,
+                        userId,
                         status,
-                    })
-                    .where(eq(subscriptions.subscriptionId, subscriptionId));
-            } else {
-                await db.insert(subscriptions).values({
-                    id: createId(),
-                    subscriptionId,
-                    userId,
-                    status,
-                });
+                    });
             }
-        }
 
-        if (event === 'subscription_updated') {
-            if (existing) {
+            if (event === "subscription_updated") {
                 await db
-                    .update(subscriptions)
-                    .set({
+                    .insert(subscriptions)
+                    .values({
+                        id: createId(),
+                        subscriptionId,
+                        userId,
                         status,
-                    })
-                    .where(eq(subscriptions.subscriptionId, subscriptionId));
-            } else {
-                await db.insert(subscriptions).values({
-                    id: createId(),
-                    subscriptionId,
-                    userId,
-                    status,
-                });
+                    });
             }
-        }
 
-        return c.json({}, 200);
-    });
+            return c.json({}, 200);
+        },
+    );
 
 export default app;
